1717595d62ef243094544ffd40eb4dc3d988d977
Polskie <mnprazmowski@gmail.com>
2023-08-02 00:42:59 -0300

    emascript

--- emascript.js
+++ emascript.js
@@ -53,7 +53,7 @@ class Product {
 const product1 = new Product("Graphics Card", "6600XT", 1999, "GC0001", 10, [])
 const product2 = new Product("Processor", "Ryzen 5", 1299, "PR0001", 7, [])
 const product3 = new Product("Memory RAM", "Corsair 3600Mhz", 990, "MR0001", 5, [])
-const product4 = new Product("Memory RA", "Corsair 3600Mz", 99, "MR0001", 6, [])
+const product4 = new Product("Memory RAM", "Corsair 3200Mhz", 99, "MR0001", 6, [])
 const productManager = new ProductManager()
 
 
